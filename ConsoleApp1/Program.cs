dd336297b314671d2fc9c77487a075d5f383a420
simon reddy kommareddy <77497059+simonreddy2001@users.noreply.github.com>
2021-05-10 14:48:17 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter a number:");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= userValue; i++)
+            {
+                for (int j = 1; j <= userValue; j++)
+                {
+                    if (i == 1 || i == userValue || j == 1 || j == userValue)
+                    {
+                        Console.Write(" *");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+    }
+}
